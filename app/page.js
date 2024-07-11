5967d1df07029a8202d23593a3550cf1b517207e
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-07-11 17:19:56 -0500

    Removed the Flask API, shifting to just front end and using the browser to send Fetch requests. Implemented basic logic for the requests.

--- app/page.js
+++ app/page.js
@@ -3,27 +3,6 @@
 import { useState } from "react";
 
 function Page() {
-  // Sample API call code
-
-  // async function getData() {
-  //   const res = await fetch("/api/pincode/", {
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //     method: "POST",
-  //     body: JSON.stringify({}),
-  //   });
-
-  //   const data = await res.json();
-
-  //   setData(data);
-  // }
-
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-
   const [uiState, setUiState] = useState(1); // toggle UI state
 
   // States to give status to the user
@@ -33,6 +12,161 @@ function Page() {
 
   // Main output state
   const [appointments, setAppointments] = useState("None");
+
+  //Login Status
+  const [loginStatus, setLoginStatus] = useState("Not Logged in");
+
+  async function checkAvailability() {
+    const zipCodesToCheck = [
+      "75001",
+      "75002",
+      "75006",
+      "75007",
+      "75009",
+      "75010",
+      "75013",
+      "75019",
+      "75020",
+      "75021",
+      "75022",
+      "75023",
+      "75024",
+      "75025",
+      "75028",
+      "75032",
+      "75034",
+      "75035",
+      "75038",
+      "75039",
+      "75040",
+      "75041",
+      "75042",
+      "75043",
+      "75044",
+      "75048",
+      "75050",
+      "75051",
+      "75052",
+      "75054",
+      "75056",
+      "75057",
+      "75058",
+      "75060",
+      "75061",
+      "75062",
+      "75063",
+      "75065",
+      "75067",
+      "75068",
+      "75069",
+      "75070",
+      "75071",
+      "75072",
+      "75074",
+      "75075",
+      "75076",
+      "75077",
+      "75078",
+      "75080",
+      "75081",
+      "75082",
+      "75087",
+      "75088",
+      "75089",
+      "75090",
+      "75092",
+      "75093",
+      "75094",
+      "75097",
+      "75098",
+      "75099",
+      "75101",
+    ];
+
+    for (const zipCode of zipCodesToCheck) {
+      const data = {
+        TypeId: typeId,
+        ZipCode: zipCode,
+        CityName: "",
+        PreferredDay: "0",
+      };
+
+      try {
+        const response = await fetch(
+          "https://publicapi.txdpsscheduler.com/api/AvailableLocation",
+          {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(data),
+          }
+        );
+
+        const locations = await response.json();
+
+        for (const location of locations) {
+          if (
+            //   location["NextAvailableDateMonth"] === "7" &&
+            //   parseInt(location["NextAvailableDateDay"]) < 9
+            1 === 1
+          ) {
+            console.log(
+              `Date is ${location["NextAvailableDate"]} at ${zipCode}, name is ${location["Name"]}`
+            );
+          }
+        }
+      } catch (error) {
+        console.log("Wrong zipcode, trying again...");
+      }
+    }
+  }
+
+  async function loginAndFetchAppointments() {
+    console.log("logging in....");
+
+    try {
+      // Login request
+      const loginResponse = await fetch(
+        "https://publicapi.txdpsscheduler.com/api/Eligibility",
+        {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(loginPayload),
+        }
+      );
+
+      const loginData = await loginResponse.json();
+      const responseId = loginData[0]?.ResponseId;
+
+      if (responseId) {
+        console.log(`Login succeeded (${responseId})...`);
+
+        // Fetch appointments
+        const bookingResponse = await fetch(
+          "https://publicapi.txdpsscheduler.com/api/Booking",
+          {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(loginPayload),
+          }
+        );
+
+        const appointments = await bookingResponse.json();
+        if (appointments.length === 0) {
+          console.log("No existing appointment found.");
+        } else {
+          console.log(
+            `Existing appointment dates: ${appointments[0]?.BookingDateTime}`
+          );
+          // Assuming `currentAppointmentDate` calculation here
+          const curAppointmentDate = new Date(appointments[0]?.BookingDateTime);
+        }
+      } else {
+        console.log("Login failed.");
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -46,7 +180,9 @@ function Page() {
     const pincodes = e.target.pincodes.value.split(",");
     const beforeDate = e.target.beforeDate.value; // this will act as a filter for the avaliable appointments
 
-    // API requests here. One pincode at a time and then update the UI so, you should need some kind of for loop here to loop over pincodes
+    const currPincode = pincodes[0];
+
+    let workingPincodes = pincodes.slice(1);
 
     setUiState(2);
   }
@@ -177,6 +313,20 @@ function Page() {
           {uiState === 2 && (
             <div className="w-full">
               <div className="relative z-0 w-full mb-5 group">
+                <label
+                  htmlFor="pincodes"
+                  className="block mb-2 mt-8 text-sm font-medium text-gray-500 dark:text-gray-400"
+                >
+                  Login Status
+                </label>
+                <div className="flex items-center justify-center w-full">
+                  <div
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 break-all mb-20"
+                    readOnly
+                  >
+                    {loginStatus}
+                  </div>
+                </div>
                 <label
                   htmlFor="pincodes"
                   className="block mb-2 text-sm font-medium text-gray-500 dark:text-gray-400"
