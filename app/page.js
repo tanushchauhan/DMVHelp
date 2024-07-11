ce1c4afb29f1ba64bc144b1ba023bdd5392328fc
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-07-11 18:23:16 -0500

    Made an API route to communicate with the DMV servers. Completed the login system and getting the existing appointment.

--- app/page.js
+++ app/page.js
@@ -16,159 +16,7 @@ function Page() {
   //Login Status
   const [loginStatus, setLoginStatus] = useState("Not Logged in");
 
-  async function checkAvailability() {
-    const zipCodesToCheck = [
-      "75001",
-      "75002",
-      "75006",
-      "75007",
-      "75009",
-      "75010",
-      "75013",
-      "75019",
-      "75020",
-      "75021",
-      "75022",
-      "75023",
-      "75024",
-      "75025",
-      "75028",
-      "75032",
-      "75034",
-      "75035",
-      "75038",
-      "75039",
-      "75040",
-      "75041",
-      "75042",
-      "75043",
-      "75044",
-      "75048",
-      "75050",
-      "75051",
-      "75052",
-      "75054",
-      "75056",
-      "75057",
-      "75058",
-      "75060",
-      "75061",
-      "75062",
-      "75063",
-      "75065",
-      "75067",
-      "75068",
-      "75069",
-      "75070",
-      "75071",
-      "75072",
-      "75074",
-      "75075",
-      "75076",
-      "75077",
-      "75078",
-      "75080",
-      "75081",
-      "75082",
-      "75087",
-      "75088",
-      "75089",
-      "75090",
-      "75092",
-      "75093",
-      "75094",
-      "75097",
-      "75098",
-      "75099",
-      "75101",
-    ];
-
-    for (const zipCode of zipCodesToCheck) {
-      const data = {
-        TypeId: typeId,
-        ZipCode: zipCode,
-        CityName: "",
-        PreferredDay: "0",
-      };
-
-      try {
-        const response = await fetch(
-          "https://publicapi.txdpsscheduler.com/api/AvailableLocation",
-          {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(data),
-          }
-        );
-
-        const locations = await response.json();
-
-        for (const location of locations) {
-          if (
-            //   location["NextAvailableDateMonth"] === "7" &&
-            //   parseInt(location["NextAvailableDateDay"]) < 9
-            1 === 1
-          ) {
-            console.log(
-              `Date is ${location["NextAvailableDate"]} at ${zipCode}, name is ${location["Name"]}`
-            );
-          }
-        }
-      } catch (error) {
-        console.log("Wrong zipcode, trying again...");
-      }
-    }
-  }
-
-  async function loginAndFetchAppointments() {
-    console.log("logging in....");
-
-    try {
-      // Login request
-      const loginResponse = await fetch(
-        "https://publicapi.txdpsscheduler.com/api/Eligibility",
-        {
-          method: "POST",
-          headers: headers,
-          body: JSON.stringify(loginPayload),
-        }
-      );
-
-      const loginData = await loginResponse.json();
-      const responseId = loginData[0]?.ResponseId;
-
-      if (responseId) {
-        console.log(`Login succeeded (${responseId})...`);
-
-        // Fetch appointments
-        const bookingResponse = await fetch(
-          "https://publicapi.txdpsscheduler.com/api/Booking",
-          {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(loginPayload),
-          }
-        );
-
-        const appointments = await bookingResponse.json();
-        if (appointments.length === 0) {
-          console.log("No existing appointment found.");
-        } else {
-          console.log(
-            `Existing appointment dates: ${appointments[0]?.BookingDateTime}`
-          );
-          // Assuming `currentAppointmentDate` calculation here
-          const curAppointmentDate = new Date(appointments[0]?.BookingDateTime);
-        }
-      } else {
-        console.log("Login failed.");
-      }
-    } catch (error) {
-      console.error("Login failed:", error);
-    }
-  }
-
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
     // information captured from the form
@@ -184,6 +32,24 @@ function Page() {
 
     let workingPincodes = pincodes.slice(1);
 
+    const res = await fetch("/api/pincode", {
+      method: "POST",
+      body: JSON.stringify({
+        options: { mode: "extAppoint" },
+        data: {
+          dateOfBirth: date,
+          firstName: first_name,
+          lastName: last_name,
+          last4ssn,
+        },
+      }),
+    });
+
+    const logincheck = await res.json();
+
+    if (!logincheck.success) return;
+
+    setLoginStatus(logincheck.data);
     setUiState(2);
   }
 
@@ -389,6 +255,14 @@ function Page() {
                 </div>
               </div>
             </div>
+          )}
+          {uiState === 2 && (
+            <button
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => setUiState(1)}
+            >
+              {"< Back"}
+            </button>
           )}
         </div>
       </div>
